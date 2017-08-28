b05b117764336d3714cd68dc954cbf198f2e8cac
chenshaosheng <ex-chenshaosheng001@pingan.com.cn>
2017-08-28 10:43:10 +0800

    3

--- demo1/src/main/java/com/css/demo1/MainActivity.java
+++ demo1/src/main/java/com/css/demo1/MainActivity.java
@@ -2,16 +2,10 @@ package com.css.demo1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Spannable;
-import android.text.Spanned;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.view.SurfaceView;
-import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import groovy.lang.GroovyShell;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,22 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView)findViewById(R.id.tv);
-        String a = (String) tv.getText();
-        Spanny spanny = new Spanny(a);
+        GroovyShell.main(new String[]{"1", "2"});
 
-        spanny.setSpan(new ClickableSpan() {
-            @Override
-            public void onClick(View widget) {
-                Toast.makeText(MainActivity.this, "11111", Toast.LENGTH_LONG).show();
-            }
-        }, 8, 10, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        tv.setMovementMethod(LinkMovementMethod.getInstance());
+        ///2
 
-        spanny.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.text_retry_flow))
-                , 8, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        tv.setText(spanny);
     }
 
 }
